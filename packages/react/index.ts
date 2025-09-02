f0ed97f2105ed85cebd63a04a13262efbfa7a6ea
firesquid6 <jdeiss06@gmail.com>
2025-09-02 12:38:05 -0500

    add react as a dependency

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,17 +1,6 @@
-import { CovenantClient } from "covenant/client";
-import { type Covenant, type ChannelMap, type ProcedureMap } from "covenant";
 
-// TODO - this will be really smart one day I promise
+export interface CovenantStore<T> {
 
-export class CovenantReactProvider<
-  P extends ProcedureMap,
-  C extends ChannelMap
-> {
-  constructor(covenant: Covenant<P, C, any, any>, client: CovenantClient<P, C, any, any>) {
-
-  }
-
-  useProcedureResult<K extends keyof P>(proc: K, inputs: P[K]) {
-
-  }
 }
+
+
