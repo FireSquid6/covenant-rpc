df7a5c5781be13b9bd133b3459cad469e69eab27
firesquid6 <jdeiss06@gmail.com>
2025-09-01 22:36:00 -0500

    remove the old covenant system

--- packages/old-covenant/adapters/vanilla.ts
+++
@@ -1,8 +0,0 @@
-import type { CovenantServer } from "../server";
-
-
-export function vanillaAdapter(server: CovenantServer<any, any, any, any, any>) {
-  return (request: Request) => {
-    return server.handle(request);
-  }
-}
