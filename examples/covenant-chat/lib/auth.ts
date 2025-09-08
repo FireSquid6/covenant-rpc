0d115af0f51d600924143d39161bb54b8966f466
firesquid6 <jdeiss06@gmail.com>
2025-09-08 15:29:06 -0500

    add github provider

--- examples/covenant-chat/lib/auth.ts
+++ examples/covenant-chat/lib/auth.ts
@@ -1,10 +1,17 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db"; 
+import { assertReadFromEnv } from "./utils";
 
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
+  socialProviders: {
+    github: {
+      clientId: assertReadFromEnv("GITHUB_CLIENT_ID"),
+      clientSecret: assertReadFromEnv("GITHUB_CLIENT_SECRET"),
+    }
+  }
 });
