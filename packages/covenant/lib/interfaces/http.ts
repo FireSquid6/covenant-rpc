5af2f9b53d03605bca746edc89f4fb9bb190efdb
firesquid6 <jdeiss06@gmail.com>
2025-09-08 22:05:28 -0500

    finish http connections

--- packages/covenant/lib/interfaces/http.ts
+++ packages/covenant/lib/interfaces/http.ts
@@ -1,5 +1,6 @@
 import type { ClientToServerConnection, ServerToSidekickConnection, SidekickToServerConnection } from ".";
 import type { ClientToSidekickConnection } from ".";
+import type { ChannelConnectionPayload, ServerMessage } from "../channel";
 import { procedureResponseSchema } from "../procedure";
 import { sidekickIncomingMessageSchema, type SidekickIncomingMessage, type SidekickOutgoingMessage } from "../sidekick/protocol";
 import { isPromise, type MaybePromise } from "../utils";
@@ -168,13 +169,102 @@ export function httpClientToServer(covenantUrl: string, extraHeaders: Record<str
 
 export function httpSidekickToServer(url: string, key: string): SidekickToServerConnection {
   return {
-    sendMessage(message) {
-      throw new Error("not implemented httpSidekickToServer");
+    async sendMessage(message) {
+      const json = JSON.stringify(message); 
+
+      const res = await fetch(url, {
+        headers: {
+          "Authorization": `Bearer ${key}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: json,
+      });
+
+      if (res.ok) {
+        return null;
+      }
+
+      return {
+        channel: message.channel,
+        params: message.params,
+        fault: "server",
+        message: `Failed to send message to server. Received: ${res.status} - ${res.statusText}`,
+      }
     },
   }
 }
 
 
+class HttpServerToSidekick implements ServerToSidekickConnection {
+  private url: URL;
+  private key: string;
+
+  constructor(url: string, key: string) {
+    this.url = new URL(url);
+    this.key = key;
+  }
+
+  async addConnection(payload: ChannelConnectionPayload): Promise<Error | null> {
+    const url = new URL(this.url.toString());
+    url.pathname = "/connection";
+
+    const res = await fetch(url.toString(), {
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.key}`,
+      },
+      method: "POST",
+    });
+
+    if (!res.ok) {
+      return new Error(`Error posting connection from sidekick: ${res.status} - ${res.statusText}`);
+    }
+    return null;
+  }
+
+  async update(resources: string[]): Promise<Error | null> {
+    const url = new URL(this.url.toString());
+    url.pathname = "/resources";
+    
+    const res = await fetch(url.toString(), {
+      body: JSON.stringify({
+        resources: resources,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.key}`,
+      },
+      method: "POST",
+    });
+
+    if (!res.ok) {
+      return new Error(`Error posting resources from sidekick: ${res.status} - ${res.statusText}`);
+    }
+    return null;
+  }
+
+  async postMessage(message: ServerMessage): Promise<Error | null> {
+    const url = new URL(this.url.toString());
+    url.pathname = "/message";
+    
+    const res = await fetch(url.toString(), {
+      body: JSON.stringify(message),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.key}`,
+      },
+      method: "POST",
+    });
+
+    if (!res.ok) {
+      return new Error(`Error posting message from sidekick: ${res.status} - ${res.statusText}`);
+    }
+    return null;
+  }
+}
+
 export function httpServerToSidekick(url: string, key: string): ServerToSidekickConnection {
-  throw new Error("Not implemented");
+  return new HttpServerToSidekick(url, key);
 }
