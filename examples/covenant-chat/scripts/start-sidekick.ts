255d239726b00c06fcf1d3cc25894d4eb867f357
firesquid6 <jdeiss06@gmail.com>
2025-09-21 18:34:26 -0500

    add the start sidekick to the covenant chat

---
+++ examples/covenant-chat/scripts/start-sidekick.ts
@@ -0,0 +1,15 @@
+import { assertReadFromEnv } from "@/lib/utils";
+import { getSidekickApi } from "@covenant/sidekick";
+
+
+// change this if you want sidekick to run on a different port
+const port = 8008;
+
+
+const secret = assertReadFromEnv("SIDEKICK_SECRET");
+const app = getSidekickApi(secret);
+
+
+app.listen(port, () => {
+  console.log(`Sidekick listening on port ${port}`); 
+});
