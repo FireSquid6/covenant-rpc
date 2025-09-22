3cc7aa8a856c2b47727997117affc973b333b60e
firesquid6 <jdeiss06@gmail.com>
2025-09-22 18:15:38 -0500

    add the methods for dealing with channels

--- examples/covenant-chat/lib/covenant.ts
+++ examples/covenant-chat/lib/covenant.ts
@@ -1,12 +1,37 @@
 import { declareCovenant, mutation, query } from "@covenant/rpc";
 import { z } from "zod";
+import { channelTableSchema, serverTableSchema } from "./db/schema";
 
 
 export const covenant = declareCovenant({
   procedures: {
     getJoinedServers: query({
       input: z.undefined(),
-      output
+      output: z.array(z.object({
+        server: serverTableSchema,
+        channels: z.array(channelTableSchema), 
+      })),
+    }),
+    createChannel: mutation({
+      input: z.object({
+        channelId: z.string(),
+      }),
+      output: channelTableSchema,
+    }),
+    deleteChannel: mutation({
+      input: z.object({
+        channelId: z.string(),
+      }),
+      output: z.undefined(),
+    }),
+    getServer: query({
+      input: z.object({
+        serverId: z.string(),
+      }),
+      output: z.object({
+        server: serverTableSchema,
+        channels: z.array(channelTableSchema),
+      }),
     }),
   },
   channels: {},
