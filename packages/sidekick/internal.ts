11fc5791dca879a67bbbc95cd3606f47ee857cc9
firesquid6 <jdeiss06@gmail.com>
2025-09-25 20:40:54 -0500

    start work on internal sidekick

---
+++ packages/sidekick/internal.ts
@@ -0,0 +1,12 @@
+import { Sidekick } from "@covenant/rpc/sidekick/index";
+
+// internal sidekick is good when you want you have a single server that can act as a sidekick and the core api (for example,
+// when you are not on the edge)
+//
+//
+// TODO - methods for sidekicks to communicate and stay in sync with each other. Need to figure out a smart way to do this.
+
+
+export class InternalSidekick {
+
+}
